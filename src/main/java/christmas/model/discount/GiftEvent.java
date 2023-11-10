8b3c2965c38a17cd7e50bb5ca4ec0e9b4f8616b2
jeondui <jeondui@gmail.com>
2023-11-11 01:08:51 +0900

    feat(GiftEvent): 증정 이벤트 객체 생성

---
+++ src/main/java/christmas/model/discount/GiftEvent.java
@@ -0,0 +1,22 @@
+package christmas.model.discount;
+
+import christmas.model.order.Date;
+
+public class GiftEvent implements DiscountEvent {
+
+    private final Date date;
+
+    public GiftEvent(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean isPossibleEvent(Date date) {
+        return date.isInRange(1, 31);
+    }
+
+    @Override
+    public int calculateDiscount(Amount totalAmount) {
+        return 0;
+    }
+}
