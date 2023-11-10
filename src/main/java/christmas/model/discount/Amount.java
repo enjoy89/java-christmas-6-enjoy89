94f83e5a653c420d8520c1b310687bd4d14d8067
jeondui <jeondui@gmail.com>
2023-11-11 01:04:31 +0900

    feat(Amount): 금액을 관리하는 일급 객체 생성

---
+++ src/main/java/christmas/model/discount/Amount.java
@@ -0,0 +1,15 @@
+package christmas.model.discount;
+
+public class Amount {
+
+    private final int totalAmount;
+
+    public Amount(int totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public int getAmount() {
+        return totalAmount;
+    }
+
+}
