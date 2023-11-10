310b95e57239e48bf60303950d151ba6b596e587
jeondui <jeondui@gmail.com>
2023-11-11 01:06:34 +0900

    feat(DiscountEvent): 할인 이벤트 인터페이스 생성 (할인 이벤트 적용 날짜인지 검사하는 메서드, 할인을 적용하는 메서드)

---
+++ src/main/java/christmas/model/discount/DiscountEvent.java
@@ -0,0 +1,11 @@
+package christmas.model.discount;
+
+import christmas.model.order.Date;
+
+public interface DiscountEvent {
+
+    boolean isPossibleEvent(Date date);
+
+    int calculateDiscount(Amount totalAmount);
+
+}
