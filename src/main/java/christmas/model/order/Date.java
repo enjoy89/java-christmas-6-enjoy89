f635994b02e6299121519341b17cb01825ba115e
jeondui <jeondui@gmail.com>
2023-11-11 04:04:17 +0900

    refactor(Date): 예외 메시지 Enum 생성 및 역할 분리

--- src/main/java/christmas/model/order/Date.java
+++ src/main/java/christmas/model/order/Date.java
@@ -1,5 +1,7 @@
 package christmas.model.order;
 
+import christmas.common.ErrorMessage;
+
 public class Date {
     private final int day;
 
@@ -14,7 +16,7 @@ public class Date {
 
     private void validateDay(int day) {
         if (day < 1 || day > 31) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            throw new IllegalArgumentException(ErrorMessage.INVALID_DATE.get());
         }
     }
 
