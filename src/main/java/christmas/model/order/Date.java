8316c05a3d1da57842c201dc0a8dff7911166e06
jeondui <jeondui@gmail.com>
2023-11-11 01:03:20 +0900

    feat(Date): 입력 날짜를 관리하는 일급 객체 생성

---
+++ src/main/java/christmas/model/order/Date.java
@@ -0,0 +1,21 @@
+package christmas.model.order;
+
+public class Date {
+    private final int day;
+
+    public Date(int day) {
+        validateDay(day);
+        this.day = day;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    private void validateDay(int day) {
+        if (day < 1 || day > 31) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+
+}
