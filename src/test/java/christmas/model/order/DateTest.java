9c322b975c60a8e3155b1659ad6356c716749342
jeondui <jeondui@gmail.com>
2023-11-11 03:52:49 +0900

    test(DateTest): Date 모델 관련 테스트 기능

---
+++ src/test/java/christmas/model/order/DateTest.java
@@ -0,0 +1,50 @@
+package christmas.model.order;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DateTest {
+
+    @DisplayName("날짜 객체 생성 테스트")
+    @Test
+    void date() {
+        int day = 1;
+        Date date = new Date(day);
+
+        assertThat(date.getDay()).isEqualTo(day);
+    }
+
+    @DisplayName("날짜 객체 생성 예외 발생 테스트")
+    @Test
+    void date2() {
+        int day = -15;
+
+        assertThatThrownBy(() -> new Date(day))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("날짜 범위가 주어졌을 때, 이 범위에 속하는지 비교 테스트")
+    @Test
+    void isInRange() {
+        int day = 10;
+        Date date = new Date(day);
+        boolean isInRange = date.isInRange(1, 25);
+
+        assertThat(isInRange).isTrue();
+    }
+
+    @DisplayName("날짜 범위가 주어졌을 때, 이 범위에 속하는지 비교 테스트")
+    @Test
+    void isInRange2() {
+        int day = 1;
+        Date date = new Date(day);
+        boolean isInRange = date.isInRange(2, 25);
+
+        assertThat(isInRange).isFalse();
+    }
+
+}
