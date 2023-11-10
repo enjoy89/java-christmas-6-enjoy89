1082f67ba455a096f59b95d4f83784b3f2c29b4d
jeondui <jeondui@gmail.com>
2023-11-11 00:23:00 +0900

    feat(Badge): 배지 Enum 생성

--- src/main/java/christmas/model/discount/Badge.java
+++ src/main/java/christmas/model/discount/Badge.java
@@ -1,4 +1,25 @@
 package christmas.model.discount;
 
-public class Badge {
+public enum Badge {
+
+    START("별", 5_000),
+    TREE("트리", 10_000),
+    SANTA("산타", 20_000);
+
+
+    private String name;
+    private int minPrice;
+
+    Badge(String name, int minPrice) {
+        this.name = name;
+        this.minPrice = minPrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMinPrice() {
+        return minPrice;
+    }
 }
