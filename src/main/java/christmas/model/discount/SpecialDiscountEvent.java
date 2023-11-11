6e070aef2ac69a51f3c08d94ac0147ea58ae05dc
jeondui <jeondui@gmail.com>
2023-11-11 17:35:40 +0900

    feat(SpecialDiscountEvent): 날짜가 특별 할인 이벤트를 적용할 수 있는지 검사하는 기능 및 할인 적용 기능

--- src/main/java/christmas/model/discount/SpecialDiscountEvent.java
+++ src/main/java/christmas/model/discount/SpecialDiscountEvent.java
@@ -1,12 +1,21 @@
 package christmas.model.discount;
 
+import christmas.common.Constant;
+import christmas.common.ErrorMessage;
 import christmas.model.order.Date;
 
 public class SpecialDiscountEvent implements DiscountEvent {
     private final Date date;
 
-    public SpecialDiscountEvent(Date date) {
+    private SpecialDiscountEvent(Date date) {
+        if (!isPossibleDate(date)) {
+            throw new IllegalArgumentException(ErrorMessage.IMPOSSIBLE_DATE_SPECIAL_EVENT.get());
+        }
         this.date = date;
+    }
+
+    public static SpecialDiscountEvent of(Date date) {
+        return new SpecialDiscountEvent(date);
     }
 
     @Override
@@ -15,12 +24,12 @@ public class SpecialDiscountEvent implements DiscountEvent {
     }
 
     @Override
-    public int calculateDiscount(Amount totalAmount) {
-        return 0;
+    public int calculateTotalDiscountAmount() {
+        return (int) Constant.INITIAL_DISCOUNT_AMOUNT.getValue();
     }
 
     private boolean isPossibleDate(Date date) {
-        return date.isInRange(1, 31);
+        return date.isSpecialDay(date.getDay());
     }
 
 }
