6832dd27c2a5ce05b86996584f09f839ed7e4571
jeondui <jeondui@gmail.com>
2023-11-11 16:08:34 +0900

    feat(Date): 날짜가 평일, 주말, 특별한 날인지 검사하는 기능

--- src/main/java/christmas/model/order/Date.java
+++ src/main/java/christmas/model/order/Date.java
@@ -1,6 +1,8 @@
 package christmas.model.order;
 
 import christmas.common.ErrorMessage;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 
 public class Date {
     private final int day;
@@ -22,5 +24,32 @@ public class Date {
 
     public boolean isInRange(int start, int end) {
         return day >= start && day <= end;
+    }
+
+    public boolean isWeekday(int day) {
+        return !isWeekend(day);
+    }
+
+    public boolean isWeekend(int day) {
+        DayOfWeek dayOfWeek = getDayOfWeek(day);
+        return dayOfWeek == DayOfWeek.FRIDAY || dayOfWeek == DayOfWeek.SATURDAY;
+
+    }
+
+    public boolean isSpecialDay(int day) {
+        return isSunday(day) || isChristmasDay(day);
+    }
+
+    private boolean isSunday(int day) {
+        DayOfWeek dayOfWeek = getDayOfWeek(day);
+        return dayOfWeek == DayOfWeek.SUNDAY;
+    }
+
+    private boolean isChristmasDay(int day) {
+        return day == 25;
+    }
+
+    private DayOfWeek getDayOfWeek(int day) {
+        return LocalDate.of(2023, 12, day).getDayOfWeek();
     }
 }
