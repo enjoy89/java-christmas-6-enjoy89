69eb208f345e87b360b24db1e722c17de09b3f29
jeondui <jeondui@gmail.com>
2023-11-11 13:59:51 +0900

    style(DateTest): given-when-then 방식 적용

--- src/test/java/christmas/model/order/DateTest.java
+++ src/test/java/christmas/model/order/DateTest.java
@@ -13,17 +13,24 @@ class DateTest {
     @DisplayName("날짜 객체 생성 테스트")
     @Test
     void date() {
+        //given
         int day = 1;
+
+        //when
         Date date = new Date(day);
 
+        //then
         assertThat(date.getDay()).isEqualTo(day);
     }
 
     @DisplayName("날짜 객체 생성 예외 발생 테스트")
     @Test
     void date2() {
+        //given
         int day = -15;
 
+        //when
+        //then
         assertThatThrownBy(() -> new Date(day))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessage.INVALID_DATE.get());
@@ -32,31 +39,42 @@ class DateTest {
     @DisplayName("날짜 범위가 주어졌을 때, 이 범위에 속하는지 비교 테스트")
     @Test
     void isInRange() {
+        //given
         int day = 10;
+
+        //when
         Date date = new Date(day);
         boolean isInRange = date.isInRange(1, 25);
 
+        //then
         assertThat(isInRange).isTrue();
     }
 
     @DisplayName("날짜 범위가 주어졌을 때, 이 범위에 속하는지 비교 테스트 - 시작 날짜보다 이전일때")
     @Test
     void isInRange2() {
+        //given
         int day = 1;
+
+        //when
         Date date = new Date(day);
         boolean isInRange = date.isInRange(2, 25);
 
+        //then
         assertThat(isInRange).isFalse();
     }
 
     @DisplayName("날짜 범위가 주어졌을 때, 이 범위에 속하는지 비교 테스트 - 끝 날자보다 후일때")
     @Test
     void isInRange3() {
+        //given
         int day = 26;
+
+        //when
         Date date = new Date(day);
         boolean isInRange = date.isInRange(2, 25);
 
+        //then
         assertThat(isInRange).isFalse();
     }
-
 }
