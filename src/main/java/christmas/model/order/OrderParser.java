9e8df4c206028085e3c6b53253fdf5e3d155ea25
jeondui <jeondui@gmail.com>
2023-11-12 04:25:26 +0900

    feat(OrderParser): 사용자로부터 입력 받은 주문을 리스트로 파싱하는 기능

---
+++ src/main/java/christmas/model/order/OrderParser.java
@@ -0,0 +1,51 @@
+package christmas.model.order;
+
+import christmas.common.ErrorMessage;
+import christmas.utils.InputValueValidator;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderParser {
+
+    public static List<OrderItem> parseOrder(String orderInfo) {
+        validateOrderFormat(orderInfo);
+        List<OrderItem> orderItems = new ArrayList<>();
+        String[] orderParts = splitOrderInfo(orderInfo);
+
+        for (String orderPart : orderParts) {
+            String[] itemParts = splitOrderItem(orderPart);
+
+            String menuName = itemParts[0];
+            int quantity = Integer.parseInt(itemParts[1]);
+
+            OrderItem orderItem = new OrderItem(menuName, quantity);
+            orderItems.add(orderItem);
+        }
+        return orderItems;
+    }
+
+    private static String[] splitOrderInfo(String orderInfo) {
+        return orderInfo.split(",");
+    }
+
+    private static String[] splitOrderItem(String orderItem) {
+        String[] parts = orderItem.split("-");
+        if (parts.length != 2) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_ORDER.get());
+        }
+        return parts;
+    }
+
+    private static void validateOrderFormat(String orderInfo) {
+        if (!orderInfo.matches("([가-힣]+-\\d+)(,[가-힣]+-\\d+)*")) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_ORDER.get());
+        }
+
+        // 이게 사용될까? 위에 조건문에서 다 걸러지는 것 같으면 삭제해야된다.
+        if (InputValueValidator.isContainsBlank(orderInfo) || InputValueValidator.isEndsWithComma(orderInfo)) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_ORDER.get());
+        }
+    }
+
+}
+
