e520c7d787263e2b140829c6013dda9d1fa76239
jeondui <jeondui@gmail.com>
2023-11-11 14:57:45 +0900

    refactor(ChristmasDiscountEvent): 크리스마스 디데이 할인 이벤트 적용 시 할인 금액을 구하는 기능 오류 수정

--- src/main/java/christmas/model/discount/ChristmasDiscountEvent.java
+++ src/main/java/christmas/model/discount/ChristmasDiscountEvent.java
@@ -1,13 +1,26 @@
 package christmas.model.discount;
 
+import christmas.common.ErrorMessage;
 import christmas.model.order.Date;
 
 public class ChristmasDiscountEvent implements DiscountEvent {
 
+    private static final int INITIAL_DISCOUNT_AMOUNT = 1000;
+    private static final int DAILY_DISCOUNT_INCREASE = 100;
+    private static final int FIRST_DATE = 1;
+    private static final int CHRISTMAS_DATE = 25;
+
     private final Date date;
 
-    public ChristmasDiscountEvent(Date date) {
+    private ChristmasDiscountEvent(Date date) {
+        if (!isPossibleEvent(date)) {
+            throw new IllegalArgumentException(ErrorMessage.IMPOSSIBLE_DATE_CHRISTMAS_EVENT.get());
+        }
         this.date = date;
+    }
+
+    public static ChristmasDiscountEvent of(Date date) {
+        return new ChristmasDiscountEvent(date);
     }
 
     @Override
@@ -16,12 +29,32 @@ public class ChristmasDiscountEvent implements DiscountEvent {
     }
 
     @Override
-    public int calculateDiscount(Amount totalAmount) {
-        return 0;
+    public int calculateTotalDiscountAmount() {
+        int dateCount = calculateDateCount(date);
+        int discountAmount = calculateDiscountAmount(dateCount);
+        return INITIAL_DISCOUNT_AMOUNT + discountAmount;
+    }
+
+    private int calculateDateCount(Date orderDate) {
+        if (isFirstDate(orderDate)) {
+            return FIRST_DATE;
+        }
+        return orderDate.getDay() - FIRST_DATE;
+    }
+
+    private int calculateDiscountAmount(int dateCount) {
+        if (dateCount == 1) {
+            return 0;
+        }
+        return dateCount * DAILY_DISCOUNT_INCREASE;
+    }
+
+    private boolean isFirstDate(Date date) {
+        return date.getDay() == 1;
     }
 
     private boolean isPossibleDate(Date date) {
-        return date.isInRange(1, 25);
+        return date.isInRange(FIRST_DATE, CHRISTMAS_DATE);
     }
 
 }
