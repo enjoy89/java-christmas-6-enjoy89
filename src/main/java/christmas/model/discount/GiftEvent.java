15c17e1fc28db9f51086f0d24bd4f7f71739a14f
jeondui <jeondui@gmail.com>
2023-11-11 18:15:47 +0900

    feat(GiftEvent): 증정 이벤트를 적용할 수 있는 검사하는 기능 및 샴페인 증정 기능

--- src/main/java/christmas/model/discount/GiftEvent.java
+++ src/main/java/christmas/model/discount/GiftEvent.java
@@ -1,26 +1,41 @@
 package christmas.model.discount;
 
+import christmas.common.Constant;
+import christmas.common.ErrorMessage;
 import christmas.model.order.Date;
+import christmas.model.order.Menu;
 
-public class GiftEvent implements DiscountEvent {
+public class GiftEvent {
 
-    private final Date date;
-
-    public GiftEvent(Date date) {
-        this.date = date;
+    private GiftEvent(Date date, Amount totalAmount) {
+        if (!isPossibleEvent(date, totalAmount)) {
+            throw new IllegalArgumentException(ErrorMessage.IMPOSSIBLE_GIFT_EVENT.get());
+        }
     }
 
-    @Override
-    public boolean isPossibleEvent(Date date) {
-        return isPossibleDate(date);
+    public static GiftEvent of(Date date, Amount totalAmount) {
+        return new GiftEvent(date, totalAmount);
     }
 
-    @Override
-    public int calculateDiscount(Amount totalAmount) {
-        return 0;
+    public int calculateTotalAmount() {
+        return getChampagne();
+    }
+
+    private boolean isPossibleEvent(Date date, Amount totalAmount) {
+        return isPossibleDate(date) && isPossibleAmount(totalAmount);
+    }
+
+
+    private int getChampagne() {
+        return Menu.DRINK_3.getPrice();
     }
 
     private boolean isPossibleDate(Date date) {
-        return date.isInRange(1, 31);
+        return date.isInRange((int) Constant.FIRST_DATE.getValue(), (int) Constant.LAST_DATE.getValue());
     }
+
+    private boolean isPossibleAmount(Amount totalAmount) {
+        return totalAmount.getAmount() >= (int) Constant.GIFT_EVENT_AMOUNT.getValue();
+    }
+
 }
