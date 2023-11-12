92379691eafb4f264ee27150a8959d1baeb600f2
jeondui <jeondui@gmail.com>
2023-11-12 12:35:59 +0900

    test(OrderInformationTest): 주문 정보의 유효성을 검증하는 기능 테스트

--- src/test/java/christmas/model/order/OrderInformationTest.java
+++ src/test/java/christmas/model/order/OrderInformationTest.java
@@ -1,7 +1,9 @@
 package christmas.model.order;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
+import christmas.common.ErrorMessage;
 import christmas.model.discount.Amount;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,8 +22,8 @@ class OrderInformationTest {
 
         //then
         int menu1_price = Menus.MAIN_3.getPrice() * 2;  // 해산물파스타 2개 가격
-        int menu2_price = Menus.DESSERT_1.getPrice() * 1; // 초코케이크 1개 가격
-        int menu3_price = Menus.DRINK_2.getPrice() * 1; // 레드와인 1개 가격
+        int menu2_price = Menus.DESSERT_1.getPrice(); // 초코케이크 1개 가격
+        int menu3_price = Menus.DRINK_2.getPrice(); // 레드와인 1개 가격
         assertThat(totalAmount.getAmount()).isEqualTo(menu1_price + menu2_price + menu3_price);
     }
 
@@ -37,9 +39,48 @@ class OrderInformationTest {
 
         //then
         int menu1_price = Menus.MAIN_3.getPrice() * 2;  // 해산물파스타 2개 가격
-        int menu2_price = Menus.DESSERT_2.getPrice() * 2; // 초코케이크 1개 가격
-        int menu3_price = Menus.DRINK_3.getPrice() * 1; // 레드와인 1개 가격
+        int menu2_price = Menus.DESSERT_2.getPrice() * 2; // 아이스크림 2개 가격
+        int menu3_price = Menus.DRINK_3.getPrice(); // 샴페인 1개 가격
         assertThat(totalAmount.getAmount()).isEqualTo(menu1_price + menu2_price + menu3_price);
+    }
+
+    @DisplayName("음료 메뉴만 입력하는 경우 예외 발생 테스트")
+    @Test
+    void validate() {
+        //given
+        String orderInfo = "제로콜라-1,레드와인-1,샴페인-1";
+
+        //when
+        //then
+        assertThatThrownBy(() -> OrderInformation.of(orderInfo))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.INVALID_ORDER.get());
+    }
+
+    @DisplayName("메뉴 수량이 1이상의 숫자가 아닌 경우 예외 발생 테스트")
+    @Test
+    void validate2() {
+        //given
+        String orderInfo = "양송이수프-0";
+
+        //when
+        //then
+        assertThatThrownBy(() -> OrderInformation.of(orderInfo))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.INVALID_ORDER.get());
+    }
+
+    @DisplayName("메뉴 수량이 20개를 초과한 경우 예외 발생 테스트")
+    @Test
+    void validate3() {
+        //given
+        String orderInfo = "양송이수프-5,티본스테이크-5,해산물파스타-5,제로콜라-10";
+
+        //when
+        //then
+        assertThatThrownBy(() -> OrderInformation.of(orderInfo))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.INVALID_ORDER.get());
     }
 
 }
