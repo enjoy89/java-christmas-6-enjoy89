860c9ae1e398c36308233320e5d74d5b2cfd86b8
jeondui <jeondui@gmail.com>
2023-11-13 21:31:36 +0900

    refactor(ChristmasDiscountEventTest): Date를 더 명확한 객체인 OrderDate로, 금액을 int에서 더 명확한 객체인 Amount로 변경

--- src/test/java/christmas/model/discount/ChristmasDiscountEventTest.java
+++ src/test/java/christmas/model/discount/ChristmasDiscountEventTest.java
@@ -1,10 +1,8 @@
 package christmas.model.discount;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-import christmas.common.ErrorMessage;
-import christmas.model.order.Date;
+import christmas.model.order.OrderDate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,37 +12,40 @@ class ChristmasDiscountEventTest {
     @Test
     void isPossibleEvent() {
         //given
-        Date date = new Date(1);
+        OrderDate date = OrderDate.of("1");
         ChristmasDiscountEvent christmasDiscountEvent = ChristmasDiscountEvent.of(date);
 
         //when
-        boolean isPossibleEvent = christmasDiscountEvent.isPossibleEvent(date);
+        Amount amount = christmasDiscountEvent.calculateTotalDiscountAmount();
 
         //then
-        assertThat(isPossibleEvent).isTrue();
+        assertThat(amount).isNotEqualTo(0);
     }
 
     @DisplayName("크리스마스 디데이 할인 이벤트를 적용할 수 없는 경우 테스트")
     @Test
     void isPossibleEvent2() {
         //given
-        Date date = new Date(26);
+        OrderDate date = OrderDate.of("26");
+        ChristmasDiscountEvent christmasDiscountEvent = ChristmasDiscountEvent.of(date);
 
-        assertThatThrownBy(() -> ChristmasDiscountEvent.of(date))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ErrorMessage.IMPOSSIBLE_DATE_CHRISTMAS_EVENT.get());
+        //when
+        Amount amount = christmasDiscountEvent.calculateTotalDiscountAmount();
+
+        //then
+        assertThat(amount.amount()).isEqualTo(0);
     }
 
     @DisplayName("12월 1일에 크리스마스 디데이 할인 이벤트 적용 시 할인 받는 금액을 구하는 기능 테스트")
     @Test
     void calculateTotalDiscountAmount() {
         //given
-        Date date = new Date(1);
-        int discountAmount = 1000; // 12월 1일에 받아야하는 할인 금액
+        OrderDate date = OrderDate.of("1");
+        Amount discountAmount = new Amount(1000); // 12월 1일에 받아야하는 할인 금액
 
         //when
         ChristmasDiscountEvent christmasDiscountEvent = ChristmasDiscountEvent.of(date);
-        int totalDiscountAmount = christmasDiscountEvent.calculateTotalDiscountAmount();
+        Amount totalDiscountAmount = christmasDiscountEvent.calculateTotalDiscountAmount();
 
         //then
         assertThat(totalDiscountAmount).isEqualTo(discountAmount);
@@ -54,12 +55,12 @@ class ChristmasDiscountEventTest {
     @Test
     void calculateTotalDiscountAmount2() {
         //given
-        Date date = new Date(10);
-        int discountAmount = 1900; // 12월 10일에 받아야하는 할인 금액
+        OrderDate date = OrderDate.of("10");
+        Amount discountAmount = new Amount(1900); // 12월 10일에 받아야하는 할인 금액
 
         //when
         ChristmasDiscountEvent christmasDiscountEvent = ChristmasDiscountEvent.of(date);
-        int totalDiscountAmount = christmasDiscountEvent.calculateTotalDiscountAmount();
+        Amount totalDiscountAmount = christmasDiscountEvent.calculateTotalDiscountAmount();
 
         //then
         assertThat(totalDiscountAmount).isEqualTo(discountAmount);
@@ -69,12 +70,12 @@ class ChristmasDiscountEventTest {
     @Test
     void calculateTotalDiscountAmount3() {
         //given
-        Date date = new Date(25);
-        int discountAmount = 3400; // 12월 25일에 받아야하는 할인 금액
+        OrderDate date = OrderDate.of("25");
+        Amount discountAmount = new Amount(3400); // 12월 25일에 받아야하는 할인 금액
 
         //when
         ChristmasDiscountEvent christmasDiscountEvent = ChristmasDiscountEvent.of(date);
-        int totalDiscountAmount = christmasDiscountEvent.calculateTotalDiscountAmount();
+        Amount totalDiscountAmount = christmasDiscountEvent.calculateTotalDiscountAmount();
 
         //then
         assertThat(totalDiscountAmount).isEqualTo(discountAmount);
