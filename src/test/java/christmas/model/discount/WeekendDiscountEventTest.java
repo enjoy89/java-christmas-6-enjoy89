d6ba8f947c1f4b338a9cb18566029f64872c2714
jeondui <jeondui@gmail.com>
2023-11-13 22:25:36 +0900

    refactor(WeekendDiscountEventTest): Date -> OrderDate로 변경 및 예외 케이스 추가

--- src/test/java/christmas/model/discount/WeekendDiscountEventTest.java
+++ src/test/java/christmas/model/discount/WeekendDiscountEventTest.java
@@ -1,13 +1,10 @@
 package christmas.model.discount;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import christmas.common.Constant;
-import christmas.common.ErrorMessage;
-import christmas.model.order.Date;
-import christmas.model.order.Menu;
-import christmas.model.order.OrderItem;
+import christmas.model.order.OrderDate;
+import christmas.model.order.OrderInformation;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,92 +14,104 @@ class WeekendDiscountEventTest {
     @Test
     void isPossibleEvent() {
         //given
-        Date date = new Date(1);
-        OrderItem orderItem = new OrderItem("티본스테이크", 1);
-        WeekendDiscountEvent weekendDiscountEvent = WeekendDiscountEvent.of(date, orderItem);
+        OrderDate date = OrderDate.of("1");
+        OrderInformation orderInformation = OrderInformation.of("티본스테이크-1");
+        WeekendDiscountEvent weekendDiscountEvent = WeekendDiscountEvent.of(date, orderInformation);
 
         //when
-        boolean isPossibleEvent = weekendDiscountEvent.isPossibleEvent(date);
+        Amount discount = weekendDiscountEvent.calculateTotalDiscountAmount();
 
         //then
-        assertThat(isPossibleEvent).isTrue();
+        assertThat(discount.amount()).isNotEqualTo(0);
     }
 
     @DisplayName("주말 할인 이벤트를 적용할 수 있는지 검사하는 기능 예외 테스트")
     @Test
     void isPossibleEvent2() {
         //given
-        Date date = new Date(4);
-        OrderItem orderItem = new OrderItem("티본스테이크", 1);
+        OrderDate date = OrderDate.of("4");
+        OrderInformation orderInformation = OrderInformation.of("티본스테이크-1");
+        WeekendDiscountEvent weekendDiscountEvent = WeekendDiscountEvent.of(date, orderInformation);
 
         //when
+        Amount discount = weekendDiscountEvent.calculateTotalDiscountAmount();
+
         //then
-        assertThatThrownBy(() -> WeekendDiscountEvent.of(date, orderItem))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ErrorMessage.IMPOSSIBLE_DATE_WEEKEND_EVENT.get());
+        assertThat(discount.amount()).isEqualTo(0);
     }
 
     @DisplayName("메인 메뉴에 주말 할인 이벤트를 적용하는 기능 테스트")
     @Test
     void calculateTotalDiscountAmount() {
         //given
-        Date date = new Date(1);
-        OrderItem orderItem = new OrderItem("티본스테이크", 1);
-        WeekendDiscountEvent weekendDiscountEvent = WeekendDiscountEvent.of(date, orderItem);
+        OrderDate date = OrderDate.of("1");
+        OrderInformation orderInformation = OrderInformation.of("티본스테이크-1");
+        WeekendDiscountEvent weekendDiscountEvent = WeekendDiscountEvent.of(date, orderInformation);
 
         //when
-        int discountAmount = weekendDiscountEvent.calculateTotalDiscountAmount();
+        Amount discount = weekendDiscountEvent.calculateTotalDiscountAmount();
 
         //then
-        assertThat(discountAmount).isEqualTo(Menu.MAIN_1.getPrice()
-                - (int) Constant.WEEKDAY_WEEKEND_DISCOUNT_AMOUNT.getValue());
+        assertThat(discount.amount()).isEqualTo(Constant.WEEKDAY_WEEKEND_DISCOUNT_AMOUNT.getValue());
     }
 
     @DisplayName("메인 메뉴에 주말 할인 이벤트를 적용하느 기능 테스트2")
     @Test
     void calculateTotalDiscountAmount2() {
         //given
-        Date date = new Date(1);
-        OrderItem orderItem = new OrderItem("바비큐립", 1);
-        WeekendDiscountEvent weekendDiscountEvent = WeekendDiscountEvent.of(date, orderItem);
+        OrderDate date = OrderDate.of("1");
+        OrderInformation orderInformation = OrderInformation.of("바비큐립-1");
+        WeekendDiscountEvent weekendDiscountEvent = WeekendDiscountEvent.of(date, orderInformation);
 
         //when
-        int discountAmount = weekendDiscountEvent.calculateTotalDiscountAmount();
+        Amount discount = weekendDiscountEvent.calculateTotalDiscountAmount();
 
         //then
-        assertThat(discountAmount).isEqualTo(Menu.MAIN_2.getPrice()
-                - (int) Constant.WEEKDAY_WEEKEND_DISCOUNT_AMOUNT.getValue());
+        assertThat(discount.amount()).isEqualTo(Constant.WEEKDAY_WEEKEND_DISCOUNT_AMOUNT.getValue());
     }
 
     @DisplayName("메인 메뉴에 주말 할인 이벤트를 적용하느 기능 테스트3")
     @Test
     void calculateTotalDiscountAmount3() {
         //given
-        Date date = new Date(1);
-        OrderItem orderItem = new OrderItem("해산물파스타", 1);
-        WeekendDiscountEvent weekendDiscountEvent = WeekendDiscountEvent.of(date, orderItem);
+        OrderDate date = OrderDate.of("1");
+        OrderInformation orderInformation = OrderInformation.of("해산물파스타-2");
+        WeekendDiscountEvent weekendDiscountEvent = WeekendDiscountEvent.of(date, orderInformation);
 
         //when
-        int discountAmount = weekendDiscountEvent.calculateTotalDiscountAmount();
+        Amount discount = weekendDiscountEvent.calculateTotalDiscountAmount();
 
         //then
-        assertThat(discountAmount).isEqualTo(Menu.MAIN_3.getPrice()
-                - (int) Constant.WEEKDAY_WEEKEND_DISCOUNT_AMOUNT.getValue());
+        assertThat(discount.amount()).isEqualTo((int) Constant.WEEKDAY_WEEKEND_DISCOUNT_AMOUNT.getValue() * 2);
     }
 
     @DisplayName("메인 메뉴에 주말 할인 이벤트를 적용하느 기능 테스트4")
     @Test
     void calculateTotalDiscountAmount4() {
         //given
-        Date date = new Date(1);
-        OrderItem orderItem = new OrderItem("크리스마스파스타", 1);
-        WeekendDiscountEvent weekendDiscountEvent = WeekendDiscountEvent.of(date, orderItem);
+        OrderDate date = OrderDate.of("1");
+        OrderInformation orderInformation = OrderInformation.of("크리스마스파스타-2");
+        WeekendDiscountEvent weekendDiscountEvent = WeekendDiscountEvent.of(date, orderInformation);
 
         //when
-        int discountAmount = weekendDiscountEvent.calculateTotalDiscountAmount();
+        Amount discount = weekendDiscountEvent.calculateTotalDiscountAmount();
 
         //then
-        assertThat(discountAmount).isEqualTo(Menu.MAIN_4.getPrice()
-                - (int) Constant.WEEKDAY_WEEKEND_DISCOUNT_AMOUNT.getValue());
+        assertThat(discount.amount()).isEqualTo((int) Constant.WEEKDAY_WEEKEND_DISCOUNT_AMOUNT.getValue() * 2);
+    }
+
+    @DisplayName("애피타이저 메뉴에 주말 할인 이벤트를 적용하는 기능 예외 테스트")
+    @Test
+    void calculateTotalDiscountAmount5() {
+        //given
+        OrderDate date = OrderDate.of("1");
+        OrderInformation orderInformation = OrderInformation.of("양송이수프-2");
+        WeekendDiscountEvent weekendDiscountEvent = WeekendDiscountEvent.of(date, orderInformation);
+
+        //when
+        Amount discount = weekendDiscountEvent.calculateTotalDiscountAmount();
+
+        //then
+        assertThat(discount.amount()).isEqualTo(0);
     }
 }
