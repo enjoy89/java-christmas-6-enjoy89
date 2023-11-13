33eef8c04eb748c67fff7e75c660869d3b531a18
jeondui <jeondui@gmail.com>
2023-11-13 22:36:44 +0900

    refactor(GiftEvent): 불필요한 로직 제거

--- src/main/java/christmas/model/discount/GiftEvent.java
+++ src/main/java/christmas/model/discount/GiftEvent.java
@@ -1,54 +1,25 @@
 package christmas.model.discount;
 
-import christmas.common.Constant;
 import christmas.common.DiscountEventName;
-import christmas.common.ErrorMessage;
-import christmas.model.order.Date;
 import christmas.model.order.Menu;
 
-public class GiftEvent {
+public class GiftEvent implements DiscountEvent {
 
-    private boolean applied;
-
-    private GiftEvent(Date date, Amount totalAmount) {
-        if (!isPossibleEvent(date, totalAmount)) {
-            throw new IllegalArgumentException(ErrorMessage.IMPOSSIBLE_GIFT_EVENT.get());
-        }
-        this.applied = false;
+    public static GiftEvent of() {
+        return new GiftEvent();
     }
 
-    public static GiftEvent of(Date date, Amount totalAmount) {
-        return new GiftEvent(date, totalAmount);
+    @Override
+    public Amount calculateTotalDiscountAmount() {
+        return new Amount(getGiftMenu());
     }
 
-    public int calculateTotalAmount() {
-        applied = true;
-        return getChampagne();
-    }
-
-    public boolean isApplied() {
-        return applied;
-    }
-
-    private boolean isPossibleEvent(Date date, Amount totalAmount) {
-        return isPossibleDate(date) && isPossibleAmount(totalAmount);
-    }
-
-
-    private int getChampagne() {
+    private int getGiftMenu() {
         return Menu.DRINK_3.getPrice();
     }
 
-    private boolean isPossibleDate(Date date) {
-        return date.isInRange((int) Constant.FIRST_DATE.getValue(), (int) Constant.LAST_DATE.getValue());
-    }
-
-    private boolean isPossibleAmount(Amount totalAmount) {
-        return totalAmount.getAmount() >= (int) Constant.GIFT_EVENT_AMOUNT.getValue();
-    }
-
-    public String getDescription() {
+    @Override
+    public String getName() {
         return DiscountEventName.GIFT_EVENT.get();
     }
-
 }
