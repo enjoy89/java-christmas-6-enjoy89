80867afe96e1a9c3699f010ba8899505187d627d
jeondui <jeondui@gmail.com>
2023-11-13 21:36:26 +0900

    test(SpecialDiscountEventTest): Date를 더 명확한 객체인 OrderDate로 변경

--- src/test/java/christmas/model/discount/SpecialDiscountEventTest.java
+++ src/test/java/christmas/model/discount/SpecialDiscountEventTest.java
@@ -1,10 +1,8 @@
 package christmas.model.discount;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-import christmas.common.ErrorMessage;
-import christmas.model.order.Date;
+import christmas.model.order.OrderDate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,39 +12,42 @@ class SpecialDiscountEventTest {
     @Test
     void isPossibleEvent() {
         //given
-        Date date = new Date(3);
+        OrderDate date = OrderDate.of("3");
         SpecialDiscountEvent specialDiscountEvent = SpecialDiscountEvent.of(date);
 
         //when
-        boolean isPossibleEvent = specialDiscountEvent.isPossibleEvent(date);
+        Amount discount = specialDiscountEvent.calculateTotalDiscountAmount();
 
         //then
-        assertThat(isPossibleEvent).isTrue();
+        assertThat(discount.amount()).isEqualTo(1000);
     }
 
     @DisplayName("특별 할인 이벤트를 적용할 수 있는지 검사하는 기능 예외 테스트")
     @Test
     void isPossibleEvent2() {
         //given
-        Date date = new Date(4);
-
-        //when
-        //then
-        assertThatThrownBy(() -> SpecialDiscountEvent.of(date))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ErrorMessage.IMPOSSIBLE_DATE_SPECIAL_EVENT.get());
-    }
-
-    @DisplayName("총 주문 금액에서 특별 할인 이벤트를 적용하는 기능 테스트")
-    @Test
-    void calculateTotalDiscountAmount() {
-        //given
-        Date date = new Date(3);
+        OrderDate date = OrderDate.of("4");
         SpecialDiscountEvent specialDiscountEvent = SpecialDiscountEvent.of(date);
 
         //when
-        int discountAmount = specialDiscountEvent.calculateTotalDiscountAmount();
+        Amount discount = specialDiscountEvent.calculateTotalDiscountAmount();
 
-        assertThat(discountAmount).isEqualTo(1000);
+        //then
+        assertThat(discount.amount()).isEqualTo(0);
     }
+
+    @DisplayName("특별 할인 이벤트를 적용할 수 있는지 검사하는 기능 예외 테스트")
+    @Test
+    void isPossibleEvent3() {
+        //given
+        OrderDate date = OrderDate.of("10");
+        SpecialDiscountEvent specialDiscountEvent = SpecialDiscountEvent.of(date);
+
+        //when
+        Amount discount = specialDiscountEvent.calculateTotalDiscountAmount();
+
+        //then
+        assertThat(discount.amount()).isNotEqualTo(0);
+    }
+
 }
