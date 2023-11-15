ff3cf94bb6495b19baafe18400b9dc5dae7c0f1f
jeondui <jeondui@gmail.com>
2023-11-15 13:59:11 +0900

    test(GiftEventTest): 증정 이벤트 할인 테스트 케이스 추가

--- src/test/java/christmas/model/discount/GiftEventTest.java
+++ src/test/java/christmas/model/discount/GiftEventTest.java
@@ -8,11 +8,24 @@ import org.junit.jupiter.api.Test;
 
 class GiftEventTest {
 
+    @DisplayName("증정 이벤트 할인 객체 생성 테스트")
+    @Test
+    void of() {
+        //given
+        Amount totalAmount = new Amount(120000);
+
+        //when
+        GiftEvent giftEvent = GiftEvent.of(totalAmount);
+
+        //then
+        assertThat(giftEvent).isNotNull();
+    }
+
     @DisplayName("증정 이벤트를 할인 금액 계산 테스트")
     @Test
     void calculateTotalDiscountAmount() {
         //given
-        Amount totalAmount = new Amount(10000);
+        Amount totalAmount = new Amount(120000);
         GiftEvent giftEvent = GiftEvent.of(totalAmount);
 
         //when
@@ -26,7 +39,7 @@ class GiftEventTest {
     @Test
     void calculateTotalDiscountAmount2() {
         //given
-        Amount totalAmount = new Amount(10000);
+        Amount totalAmount = new Amount(120000);
         GiftEvent giftEvent = GiftEvent.of(totalAmount);
 
         //when
@@ -34,6 +47,20 @@ class GiftEventTest {
 
         //then
         assertThat(amount.amount()).isEqualTo(Menu.DRINK_3.getPrice()); // 샴페인 가격
+    }
+
+    @DisplayName("할인 전 총주문 금액이 12만원 미만일 때 할인 이벤트 미적용 기능 테스트")
+    @Test
+    void calculateTotalDiscountAmount4() {
+        //given
+        Amount totalAmount = new Amount(110000);
+        GiftEvent giftEvent = GiftEvent.of(totalAmount);
+
+        //when
+        Amount amount = giftEvent.getBenefit();
+
+        //then
+        assertThat(amount.amount()).isEqualTo(0);
     }
 
 }
