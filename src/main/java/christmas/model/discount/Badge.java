eaf76fb476c05e66f5361793dba3a038cb6543b4
jeondui <jeondui@gmail.com>
2023-11-15 13:42:29 +0900

    refactor(Badge): 오타 수정 및 기본값 배지 생성

--- src/main/java/christmas/model/discount/Badge.java
+++ src/main/java/christmas/model/discount/Badge.java
@@ -1,11 +1,10 @@
 package christmas.model.discount;
 
 public enum Badge {
-
-    START("별", 5_000),
+    STAR("별", 5_000),
     TREE("트리", 10_000),
-    SANTA("산타", 20_000);
-
+    SANTA("산타", 20_000),
+    NONE("없음", 0);
 
     private String name;
     private int minPrice;
@@ -21,5 +20,9 @@ public enum Badge {
 
     public int getMinPrice() {
         return minPrice;
+    }
+
+    public static Badge getDefault() {
+        return NONE;
     }
 }
