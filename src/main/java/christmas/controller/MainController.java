76278e2ffda62c5bed54ac9f29cb1f080df5cd1a
jeondui <jeondui@gmail.com>
2023-11-15 15:06:27 +0900

    feat(MainController): 예외 처리 후 재입력 받도록 수정

--- src/main/java/christmas/controller/MainController.java
+++ src/main/java/christmas/controller/MainController.java
@@ -1,6 +1,8 @@
 package christmas.controller;
 
 import christmas.model.discount.Amount;
+import christmas.model.discount.Badge;
+import christmas.model.discount.Benefit;
 import christmas.model.order.Order;
 import christmas.model.order.OrderDate;
 import christmas.model.order.OrderInformation;
@@ -10,59 +12,79 @@ import christmas.view.OutputView;
 public class MainController {
 
     public void start() {
-
         printStartInstructions();
 
-        OrderDate orderDate = getOrderDate(InputView.inputVisitDate());
-        OrderInformation orderInformation = getOrderInformation(InputView.inputOrderInformation());
+        OrderDate orderDate = getOrderDate();
+        OrderInformation orderInformation = getOrderInformation();
 
         Order order = createOrder(orderDate, orderInformation);
-
-        //4. 할인 전 총주문 금액 출력
         Amount totalAmount = orderInformation.getTotalAmount();
-        OutputView.printTotalAmountBeforeDiscount(totalAmount);
+        printTotalAmount(totalAmount);
 
-        applyDiscount(order);
+        Amount totalBenefit = applyDiscount(order);
+        printTotalBenefitAmount(totalBenefit);
 
         Amount totalBenefitExceptGiftBenefit = order.getTotalBenefitExceptGiftBenefit();
         OutputView.printAmountAfterDiscount(getEstimatedPaymentAmount(totalAmount, totalBenefitExceptGiftBenefit));
-    }
-
-    private Order createOrder(OrderDate orderDate, OrderInformation orderInformation) {
-        // 3. 주문 메뉴를 출력한다.
-        printOrderMenus(orderDate, orderInformation);
-        return new Order(orderDate, orderInformation);
-    }
-
-    private void applyDiscount(Order order) {
-        printGiftEvent(order);
-        OutputView.printBenefitsDetails(order.getAppliedDiscountEvents());
-        Amount totalBenefit = order.getTotalBenefit();
-        OutputView.printTotalBenefitAmount(totalBenefit);
+        printBadge(totalBenefit);
     }
 
     private void printGiftEvent(Order order) {
         OutputView.printGiftMenu(order.getGiftEventBenefit());
     }
 
-    private Amount getEstimatedPaymentAmount(Amount totalAmount, Amount totalBenefitExceptGiftBenefit) {
-        return new Amount(totalAmount.amount() - totalBenefitExceptGiftBenefit.amount());
-    }
-
     private void printStartInstructions() {
         OutputView.printStartInstructions();
     }
 
-    private OrderDate getOrderDate(String input) {
-        return OrderDate.of(input);
+    private OrderDate getOrderDate() {
+        try {
+            return OrderDate.of(InputView.inputVisitDate());
+        } catch (IllegalArgumentException exception) {
+            OutputView.printErrorMessage(exception.getMessage());
+            return getOrderDate();
+        }
     }
 
-    private OrderInformation getOrderInformation(String input) {
-        return OrderInformation.of(input);
+    private OrderInformation getOrderInformation() {
+        try {
+            return OrderInformation.of(InputView.inputOrderInformation());
+        } catch (IllegalArgumentException exception) {
+            OutputView.printErrorMessage(exception.getMessage());
+            return getOrderInformation();
+        }
+    }
+
+    private Order createOrder(OrderDate orderDate, OrderInformation orderInformation) {
+        printOrderMenus(orderDate, orderInformation);
+        return new Order(orderDate, orderInformation);
     }
 
     private void printOrderMenus(OrderDate orderDate, OrderInformation orderInformation) {
         OutputView.printPreviewInstructions(orderDate.getDate());
         OutputView.printOrderMenus(orderInformation.getOrderItems());
+    }
+
+    private void printTotalAmount(Amount totalAmount) {
+        OutputView.printTotalAmountBeforeDiscount(totalAmount);
+    }
+
+    private Amount applyDiscount(Order order) {
+        printGiftEvent(order);
+        OutputView.printBenefitsDetails(order.getAppliedDiscountEvents());
+        return order.getTotalBenefit();
+    }
+
+    private void printTotalBenefitAmount(Amount totalBenefit) {
+        OutputView.printTotalBenefitAmount(totalBenefit);
+    }
+
+    private Amount getEstimatedPaymentAmount(Amount totalAmount, Amount totalBenefitExceptGiftBenefit) {
+        return new Amount(totalAmount.amount() - totalBenefitExceptGiftBenefit.amount());
+    }
+
+    private void printBadge(Amount totalBenefit) {
+        Benefit benefit = Benefit.of(totalBenefit);
+        OutputView.printEventBadge(benefit.getBadge());
     }
 }
